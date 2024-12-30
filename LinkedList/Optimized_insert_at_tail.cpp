69495a8eef477bd0952dff506f1e5bc7c353647b
Saleh Rafi <salehrafishyam@gmail.com>
2024-12-31 00:55:06 +0600

    optimized code added

---
+++ LinkedList/Optimized_insert_at_tail.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node* next;
+    Node(int data){
+        this->data=data;
+        this->next = NULL;
+    }
+};
+
+void printLinkedList(Node* head){
+    Node* temp;
+    temp  = head;
+    while(temp!=NULL){
+        cout << temp->data << endl;
+        temp = temp->next;
+    }
+}
+
+void insert_at_tail(Node* &head, Node* &tail, int data){
+    Node* newNode = new Node(data);
+    if(head == NULL){
+        head = newNode;
+        return;
+    }
+    tail->next = newNode;
+    tail = tail->next;
+}
+
+int main(int argc, char const *argv[])
+{
+     Node* head = new Node(10);
+    Node* a = new Node(20);
+    Node* tail = new Node(30);
+
+    head->next = a;
+    a->next = tail;
+
+    insert_at_tail(head,tail,202);
+    insert_at_tail(head,tail,404);
+    insert_at_tail(head,tail,500);
+    printLinkedList(head);
+    return 0;
+}
