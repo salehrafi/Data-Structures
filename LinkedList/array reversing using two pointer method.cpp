9e883395f2245d52fb631668ef930046cd249515
Saleh Rafi <117371849+salehrafi@users.noreply.github.com>
2025-02-02 21:07:39 +0600

    Update array reversing using two pointer method.cpp

--- LinkedList/array reversing using two pointer method.cpp
+++ LinkedList/array reversing using two pointer method.cpp
@@ -4,11 +4,11 @@
 using namespace std;
 int main(int argc, char const *argv[])
 {
-    int n=5;
-    int arr[n] = {1,2,3,4,5};
+    int n=6;
+    int arr[n] = {1,2,3,4,5,6};
     
     int i=0,j=n-1;
-    while(i!=j){
+    while(i<=j){
         swap(arr[i],arr[j]);
         i++;j--;
     }
